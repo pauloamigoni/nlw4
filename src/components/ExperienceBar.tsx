3fcf95fe81f85c6dd2862204024d0b68a4964114
Amigoni <paulo.amigoni@gmail.com>
2021-02-22 11:19:14 -0300

    Adicionando Icons

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,15 @@
+import { ImStarEmpty, ImStarFull, ImStarHalf } from 'react-icons/im';
+
+
 export function ExperienceBar(){
     return(
         <header className="experience-bar">
-            <span>0 xp</span>
+            <span>0 <ImStarEmpty /></span>
             <div>
                 <div style={{ width: '50%'}} />
-                <span className="current-experience" style={{ left: '50%' }}>300 xp</span>
+                <span className="current-experience" style={{ left: '50%' }}>300 <ImStarHalf /></span>
             </div>
-            <span>600 px</span>
+            <span>600 <ImStarFull /></span>
 
         </header>
     )
